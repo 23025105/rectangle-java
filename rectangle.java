0b6493029907282f9bddfae441c84648c560a89e
23025105 <167212327+23025105@users.noreply.github.com>
2025-03-06 23:01:03 +0200

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,15 @@
+public class rectangle
+{
+  public static void main(String [] args)
+    {
+        int Area,perimeter;
+        int Lenght = 5;
+        int Breath = 2;
+        
+        Area=(Lenght*Breath);
+        perimeter=(Lenght+Breath)*2;
+        
+        System.out.println("Area="+Area);
+        System.out.print("perimeter="+perimeter);
+    }
+}
